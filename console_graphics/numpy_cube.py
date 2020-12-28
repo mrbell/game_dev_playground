a0bf2ae9e13a43a675f5c466bd9b1b09b01fe2b0
Michael Bell <bellmr@gmail.com>
2020-12-28 18:07:14 -0500

    just using numpy without overhauling the game loop didn't work.

--- console_graphics/numpy_cube.py
+++ console_graphics/numpy_cube.py
@@ -1,6 +1,13 @@
 '''
 Draw a cube to the terminal and rotate the camera around the cube at a fixed rate. 
 Trying to do it in pure Python and hit at least 20 FPS.
+
+This is a version using numpy and it runs much slower actually, I think because
+of the extra overhead. Naive implementation here includes MANY small vector operations.
+I am confident I could optimize by doing batch operations (effectively vectorizing
+my loops), but I don't think that's what I really want to do. If I wanted to optimize
+through bypassing Python, I'd just write the damn thing in C... maybe not a bad idea anyway.
+So I'm abandoning this idea.
 '''
 import time
 import curses
@@ -32,49 +39,19 @@ def z_rotate(vector, angle_rad):
     cosa = np.cos(angle_rad)
     sina = np.sin(angle_rad)
 
-    return Vector(*(np.array([[cosa, -sina, 0], [sina, cosa, 0], [0, 0, 1]]) @ vector.xyz))
+    return np.array([[cosa, -sina, 0], [sina, cosa, 0], [0, 0, 1]]) @ vector
 
 
-class Vector(object):
-    '''
-    A basic 3D Vector class. Supports basic operations like addition, subtraction,
-    scalar multiplication, dot product, cross product, normalization, and length
-    calculation. Can be compared to other vectors for equality.
-    '''
-    __slots__ = 'xyz', '_length'
-    def __init__(self, x, y, z):
-        self.xyz = np.array([x, y, z])
-        self._length = None
-    def copy(self):
-        return Vector(*self.xyz)
-    def __add__(self, other_vector):
-        return Vector(*(self.xyz + other_vector.xyz))
-    def __sub__(self, other_vector):
-        return Vector(*(self.xyz - other_vector.xyz))
-    def __mul__(self, scale_factor):
-        return Vector(*(self.xyz * scale_factor))
-    def __rmul__(self, scale_factor):
-        return self.__mul__(scale_factor)
-    def dot(self, other_vector):
-        return self.xyz @ other_vector.xyz
-    def cross(self, other_vector):
-        # return Vector(*np.cross(self.xyz, other_vector.xyz))
-        return Vector(
-            self.xyz[1] * other_vector.xyz[2] - self.xyz[2] * other_vector.xyz[1],
-            -self.xyz[0] * other_vector.xyz[2] + self.xyz[2] * other_vector.xyz[0],
-            self.xyz[0] * other_vector.xyz[1] - self.xyz[1] * other_vector.xyz[0]
-        )
-    def __eq__(self, other_vector):
-        return self.xyz == other_vector.xyz
-    def length(self):
-        if self._length is None:
-            self._length = np.linalg.norm(self.xyz)
-        return self._length
-    def normalize(self):
-        my_length = self.length()
-        return Vector(*(self.xyz / my_length))
-    def __repr__(self):
-        return f"Vector(x={self.xyz[0]}, y={self.xyz[1]}, z={self.xyz[2]})"
+def normalize(vec):
+    return vec / np.linalg.norm(vec)
+
+
+def cross_prod(veca, vecb):
+    return np.array([
+        veca[1] * vecb[2] - veca[2] * vecb[1],
+        -veca[0] * vecb[2] + veca[2] * vecb[0],
+        veca[0] * vecb[1] - veca[1] * vecb[0]
+    ])
 
 
 class Poly(object):
@@ -96,7 +73,7 @@ class Poly(object):
             edges.append(v2 - v1)
         self.edges = edges
 
-        self.surface_vector = edges[1].cross(edges[2]).normalize()
+        self.surface_vector = normalize(cross_prod(edges[1], edges[2]))
 
     def __add__(self, other_vector):
         vertices = []
@@ -111,12 +88,12 @@ class Poly(object):
         '''
         Check whether a given point (as a Vector) is within the poly face.
         '''
-        cross = self.edges[0].cross(point - self.vertices[-1])
+        cross = cross_prod(self.edges[0], (point - self.vertices[-1]))
+        
         for edge, edge_base in zip(self.edges[1:], self.vertices[:-1]):
             to_point = point - edge_base
-            # EPSILON here protects from points along an edge of the poly from getting registered
-            # improperly
-            if cross.dot(edge.cross(to_point)) <= 0:
+
+            if cross @ cross_prod(edge, to_point) <= 0:
                 return False
         return True
 
@@ -125,10 +102,10 @@ class Poly(object):
         Calculate the point where the given ray (defined by a base vector and a unit vector along
         the ray) intersects with the plane of the poly face.
         '''
-        denom = self.surface_vector.dot(unit_vec)
+        denom = self.surface_vector @ unit_vec
         if abs(denom) < EPS:
             return None
-        param = self.surface_vector.dot(self.vertices[0] - base) / denom
+        param = (self.surface_vector @ (self.vertices[0] - base)) / denom
 
         if param < 0:
             return None
@@ -154,43 +131,43 @@ def main(screen):
     # a norm vector that points outside of the cube
     cube_polys = [
         Poly([     # X-Y plane face at Z = 0
-            Vector(0, 0, 0),
-            Vector(0, 1, 0),
-            Vector(1, 1, 0),
-            Vector(1, 0, 0),
+            np.array([0, 0, 0]),
+            np.array([0, 1, 0]),
+            np.array([1, 1, 0]),
+            np.array([1, 0, 0]),
         ]), Poly([  # X-Y plane face at Z = 1
-            Vector(0, 0, 1),
-            Vector(1, 0, 1),
-            Vector(1, 1, 1),
-            Vector(0, 1, 1),
+            np.array([0, 0, 1]),
+            np.array([1, 0, 1]),
+            np.array([1, 1, 1]),
+            np.array([0, 1, 1]),
         ]), Poly([  # Y-Z plane face at X = 0
-            Vector(0, 0, 0),
-            Vector(0, 0, 1),
-            Vector(0, 1, 1),
-            Vector(0, 1, 0),
+            np.array([0, 0, 0]),
+            np.array([0, 0, 1]),
+            np.array([0, 1, 1]),
+            np.array([0, 1, 0]),
         ]), Poly([  # Y-Z plane face at X = 1
-            Vector(1, 0, 0),
-            Vector(1, 1, 0),
-            Vector(1, 1, 1),
-            Vector(1, 0, 1),
+            np.array([1, 0, 0]),
+            np.array([1, 1, 0]),
+            np.array([1, 1, 1]),
+            np.array([1, 0, 1]),
         ]), Poly([  # X-Z plane face at Y = 0
-            Vector(0, 0, 0),
-            Vector(1, 0, 0),
-            Vector(1, 0, 1),
-            Vector(0, 0, 1),
+            np.array([0, 0, 0]),
+            np.array([1, 0, 0]),
+            np.array([1, 0, 1]),
+            np.array([0, 0, 1]),
         ]), Poly([  # X-Z plane face at Y = 1
-            Vector(0, 1, 0),
-            Vector(0, 1, 1),
-            Vector(1, 1, 1),
-            Vector(1, 1, 0),
+            np.array([0, 1, 0]),
+            np.array([0, 1, 1]),
+            np.array([1, 1, 1]),
+            np.array([1, 1, 0]),
         ])
     ]
 
     # TESTING
     # cube_polys = [cube_poly + Vector(0, 0, 1000) for cube_poly in cube_polys]
 
-    player_pos = Vector(np.sqrt(2) / 2 * PLAYER_DIST, np.sqrt(2) / 2 * PLAYER_DIST, PLAYER_HEIGHT)
-    player_dir = (Vector(0, 0, PLAYER_HEIGHT) - player_pos).normalize()
+    player_pos = np.array([np.sqrt(2) / 2 * PLAYER_DIST, np.sqrt(2) / 2 * PLAYER_DIST, PLAYER_HEIGHT])
+    player_dir = normalize(np.array([0, 0, PLAYER_HEIGHT]) - player_pos)
 
     screen_height, screen_width = screen.getmaxyx()
     center_screen_row, center_screen_col = screen_height // 2, screen_width // 2
@@ -198,14 +175,14 @@ def main(screen):
     v_pix_size = 2 * SCREEN_DISTANCE * np.tan(VFOV / 2) / screen_height
     h_pix_size = 2 * SCREEN_DISTANCE * np.tan(HFOV / 2) / screen_width
 
-    light_location = Vector(0.5, LIGHT_DISTANCE, 0.5)
+    light_location = np.array([0.5, LIGHT_DISTANCE, 0.5])
     n = 0
     while True: # n < 10:
         n += 1
         
         t0 = time.time()
 
-        view_angle = np.arctan2(player_dir.xyz[1], player_dir.xyz[0])
+        view_angle = np.arctan2(player_dir[1], player_dir[0])
         cosa = np.cos(view_angle)
         sina = np.sin(view_angle)
         R = np.array([[cosa, -sina, 0], [sina, cosa, 0], [0, 0, 1]])
@@ -215,19 +192,19 @@ def main(screen):
                 # In coordinate frame where y, z is defined by screen, y horiz L-R, z vert down to up
                 # x is out of screen away from player
                 # player is at (x,y,z) = (0, 0, 0)
-                pix_vec = Vector(
+                pix_vec = normalize(np.array([
                     SCREEN_DISTANCE,
                     (col - center_screen_col) * h_pix_size,
                     (center_screen_row - row) * v_pix_size  # Because curses y=0 is top of screen
-                ).normalize()
-                pix_vec = Vector(*(R @ pix_vec.xyz))
+                ]))
+                pix_vec = R @ pix_vec
 
                 polys_along_los = []
 
                 for poly in cube_polys:
                     plane_point = poly.ray_intersection(player_pos, pix_vec)
                     if plane_point is not None:
-                        plane_dist = (plane_point - player_pos).length()
+                        plane_dist = np.linalg.norm(plane_point - player_pos)
                         polys_along_los.append((plane_dist, plane_point, poly))
 
                 pixel_selector = 0
@@ -236,9 +213,9 @@ def main(screen):
                     dist_to_point, point_to_draw, poly = min(polys_along_los, key=lambda x: x[0])
                     poly_to_light = light_location - point_to_draw
 
-                    dist_to_light = (poly_to_light).length()
+                    dist_to_light = np.linalg.norm(poly_to_light)
 
-                    if poly.surface_vector.dot(poly_to_light) > 0:
+                    if poly.surface_vector @ poly_to_light > 0:
                         light_reflection = LIGHT_BRIGHTNESS #- 2 * dist_to_light
                     else:
                         light_reflection = 0
